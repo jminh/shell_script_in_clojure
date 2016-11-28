8251177641eb4196ecf8d2d31d65214c37ececf4
ming <archaicdust@gmail.com>
2016-11-28 23:40:31 +0800

    initial commit

---
+++ main.clj
@@ -0,0 +1,10 @@
+'{:dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.palletops/stevedore "0.8.0-beta.7"]]}
+
+(require '[pallet.stevedore :refer [script with-script-language]])
+(require 'pallet.stevedore.bash) ;; for bash output
+
+(with-script-language :pallet.stevedore.bash/bash
+  (script
+   (doseq [x ["a" "b" "c"]]
+     (println @x))))
